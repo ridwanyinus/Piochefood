7d258f3552ad3669ab8744b4435c81d1b5898a1c
officialyaro9 <ridwanyinusa15@gmail.com>
2023-09-10 14:39:08 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["./dist/**/*.{html,js}"],
+   theme: {
+      extend: {
+         colors: {
+            primary: "#ff6363",
+            secondary: {
+               100: "#E2E2D5",
+               200: "#888883",
+            },
+         },
+
+         fontFamily: {
+            body: ["Nunito"],
+         },
+      },
+   },
+   plugins: [],
+};
